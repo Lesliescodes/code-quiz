f43534f5a7e862d4d6c48535522ccef31e032941
Leslie Codes <84750526+Lesliescodes@users.noreply.github.com>
2021-11-19 21:29:22 -0700

    New script

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+var javaScriptQuestions = [
+{
+    question: "What is a String",
+    possibles: ["data type","a piece of fabric", "also known as shoe lace"],
+    answer: "data type",
+},
+{
+    question: "Which syntax is used on a string",
+    possibles: ["curly brackets","quotes", "parentheses"],
+    answer: "quotes",
+},
+{
+    question: "Fast way to check if code is working?",
+    possibles: ["add plus sign to end of string","console.log", "put on a timer"],
+    answer: "data type",
+},
+{
+    question: "What is a String",
+    possibles: ["data type","a piece of fabric", "also known as shoe lace"],
+    answer: "data type",
+},
+
+]
